344298c71eb99ddcba7314e2ec4800ae884b1d22
ycyoon <ycyoon@gmail.com>
2022-09-07 16:17:34 +0900

    swin transformer 추가

---
+++ train_swin.sh
@@ -0,0 +1,24 @@
+python train_swin.py --gpu 0 --epochs 200 --batch_size 32 --lr 1e-3 --dropout 0.1 \
+	--dataset_name "merged" \
+	--dataset_dir "data" \
+	--workers 4 --vid_frame 150 --vid_frame_stride 2 \
+	--log_dir "./logs/merged/test" \
+	--model_depth 50 \
+
+
+
+#python test.py --gpu 0 --epochs 100 --batch_size 128 --lr 5e-3 --dropout 0 \
+#	--pretrained "model/ubfc_best.pth" \
+#	--dataset_name "ubfc-rppg" \
+#	--dataset_dir "data" \
+#	--workers 4 --vid_frame 150 --vid_frame_stride 2 \
+#	--log_dir "./logs/ubfc/test" \
+#	--model_depth 18
+
+#python test.py --gpu 0 --epochs 100 --batch_size 128 --lr 5e-3 --dropout 0 \
+#	--pretrained "model/mahnob_best.pth" \
+#	--dataset_name "mahnob-hci" \
+#	--dataset_dir "data2" \
+#	--workers 4 --vid_frame 150 --vid_frame_stride 2 \
+#	--log_dir "./logs/mahnob/test" \
+#	--model_depth 18
